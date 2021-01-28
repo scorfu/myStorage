8424e733738cd2e0e3ff34ff2323c750a05da602
Stefania Popa <stefania_florina10@yahoo.com>
2021-01-28 17:54:12 +0200

    load data from json

--- functions.js
+++ functions.js
@@ -1,15 +1,30 @@
+console.log('test script')
+
+
+function insertObj(object){
+    const tbody = document.querySelector('#list tbody');
+    tbody.innerHTML = getObjsHtml(object);
+}
+
+function getObjsHtml(object){
+    var rHtml = object.map(objects => {
+        return getObjHtml(objects)
+    });
+    return rHtml.join("");
+}
+
+function getObjHtml(object){
+    return  `<tr>
+    <td>${object.nameObj}</td>
+    <td>${object.category}</td>
+</tr>`;
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-fetch("myStorage/data.json").then(r => r.json())
+fetch("data.json")
+    .then(r => r.json())
+    .then(data => {
+        insertObj(data);
+    });
