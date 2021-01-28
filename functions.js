a7dba49ffec0d9fb7aee29aafe5d22da4dd89e78
scorfu <sergiucorfu@yahoo.com>
2021-01-28 22:03:40 +0200

    added basic search function for name&cat.

--- functions.js
+++ functions.js
@@ -20,10 +20,30 @@ function getObjHtml(object){
 }
 
 
-
-
 fetch("data.json")
     .then(r => r.json())
     .then(data => {
+        allObjs = data
         insertObj(data);
     });
+
+let allObjs = [];
+
+function searchObjs(text) {
+    text = text.toLowerCase();
+    return allObjs.filter(obj => {
+        return obj.nameObj.toLowerCase().indexOf(text) > -1 ||
+            obj.category.toLowerCase().indexOf(text) > -1;
+    });
+}
+
+function addEventListeners() {
+    const search = document.getElementById('search');
+    search.addEventListener("input", e => {
+        const text = e.target.value;
+        const filtrate = searchObjs(text);
+        console.info({ filtrate })
+        insertObj(filtrate);
+    });
+}
+addEventListeners();
