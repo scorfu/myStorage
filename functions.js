fdab63d752202819464c201f7b4e646419dea019
scorfu <sergiucorfu@yahoo.com>
2021-01-30 16:55:19 +0200

    manual create & delete

--- functions.js
+++ functions.js
@@ -1,5 +1,24 @@
 console.log('test script')
 
+const API = {
+    CREATE: {
+        URL:"create.json",
+        METHOD: "GET"
+    },
+    READ: {
+        URL:"data.json",
+        METHOD: "GET"
+    },
+    UPDATE: {
+        URL:"",
+        METHOD: "GET"
+    },
+    DELETE: {
+        URL:"delete.json",
+        METHOD: "GET"
+    },
+}
+
 function insertObj(object){
     const tbody = document.querySelector('#list tbody');
     tbody.innerHTML = getObjsHtml(object);
@@ -15,16 +34,22 @@ function getObjHtml(object){
     <td>${object.category}</td>
     <td>${object.depositArea}</td>
     <td>${object.depositDate}</td>
+    <td>
+        <a href="${API.DELETE.URL}?id=${object.id}">&#128465</a>
+    <td>
 </tr>`;
 }
 
-
-fetch("data.json")
+function loadList() {
+    fetch(API.READ.URL)
     .then(r => r.json())
     .then(data => {
         allObjs = data
         insertObj(data);
     });
+}
+
+loadList();
 
 let allObjs = [];
 
@@ -52,7 +77,26 @@ function saveObj () {
     const category = document.querySelector("#category option:checked").value;
     const depositArea = document.querySelector("#depositArea option:checked").value;
     const depositDate = document.querySelector("#list input[name=depositDate]").value;
-    console.log("saving", nameObj, category, depositArea, depositDate)
+    const obj = {
+        nameObj,
+        category,
+        depositArea,
+        depositDate
+    }
+    console.info("saving", nameObj, category, depositArea, depositDate);
+    console.log(obj)
+
+    fetch(API.CREATE.URL, {
+        method: API.CREATE.METHOD,
+        body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(obj)
+    })
+        .then(res => res.json())
+        .then(r => {
+            console.warn(r);
+            if (r.success) {
+                loadList();
+            }
+        });
 };
 
 const saveBtn = document.querySelector("#saveBtn");
