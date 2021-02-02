398a270708797397659dabf8405449c83317e6ec
Stefania Popa <stefania_florina10@yahoo.com>
2021-02-02 13:53:42 +0200

    add filter button

--- functions.js
+++ functions.js
@@ -102,3 +102,8 @@ saveBtn.addEventListener("click", () => {
 
 loadList();
 addEventListeners();
+
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
