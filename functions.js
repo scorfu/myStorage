2c6bc9cecb2fb09772912930f79de7812891691f
Stefania Popa <stefania_florina10@yahoo.com>
2021-03-02 18:00:36 +0200

    did click out on deopdown btn

--- functions.js
+++ functions.js
@@ -256,6 +256,24 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+  
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+
+  document.getElementById("myDropdown").addEventListener('click',function(event){
+    event.stopPropagation();
+});
+
   // end of Category Seach button
 
  function reset(){
@@ -266,3 +284,4 @@ function myFunction() {
     })
     )
  }
+
